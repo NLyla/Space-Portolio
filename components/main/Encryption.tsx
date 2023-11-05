b9a3e55716601b923f6a9fe3341b690a2c3c2b5d
Lyla <146758880+NLyla@users.noreply.github.com>
2023-11-06 06:10:27 +0700

    Update Encryption.tsx

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -41,16 +41,17 @@ const Encryption = () => {
         </div>
 
         <div className="Welcome-box px-[15px] py-[4px] z-[20] brder my-[20px] border-[#7042f88b] opacity-[0.9]">
-          <h1 className="Welcome-text text-[12px]">Encryption</h1>
+          <h1 className="Welcome-text text-[12px]">Encryption  
+          <CProgress color="success" variant="striped" animated value={25} />
+            <CProgress color="info" variant="striped" animated value={50} />
+            <CProgress color="warning" variant="striped" animated value={75} />
+            <CProgress color="danger" variant="striped" animated value={100} />
+          </h1>
         </div>
       </div>
       <div className="absolute z-[20] bottom-[10px] px-[5px]">
         <div className="cursive text-[20px] font-medium text-center text-gray-300">
           Secure your data with end-to-end encryption
-          <CProgress color="success" variant="striped" animated value={25} />
-            <CProgress color="info" variant="striped" animated value={50} />
-            <CProgress color="warning" variant="striped" animated value={75} />
-            <CProgress color="danger" variant="striped" animated value={100} />
         </div>
       </div>
 
