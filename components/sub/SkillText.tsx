f2566c3b33c2db47334a05937646b0de7b06e1cf
Lyla <146758880+NLyla@users.noreply.github.com>
2023-11-06 06:17:14 +0700

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import {motion} from 'framer-motion'
 import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/24/solid'
-
+import { CProgress } from "@coreui/react";
 const SkillText = () => {
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center'>
@@ -21,6 +21,7 @@ const SkillText = () => {
         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
         >
             Making apps with modern technologies
+          <CProgress value={25}>25%</CProgress>
         </motion.div>
         <motion.div
         variants={slideInFromRight(0.5)}
