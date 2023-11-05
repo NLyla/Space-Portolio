71a5efd48962e512d22f24e69061b9c23ec7e80b
Lyla <146758880+NLyla@users.noreply.github.com>
2023-11-05 13:36:43 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Encryption from "@/components/main/Encryption";
+import MusiCard from "@/components/main/FavMusic";
 import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
-        
+        <MusiCard />
         <Projects />
       </div>
     </main>
