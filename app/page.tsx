c0a4eaffe5e10996281dbac493a1111bff4ee57e
Lyla <146758880+NLyla@users.noreply.github.com>
2023-11-06 01:52:45 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
+import DefaultProgressBar from "@/components/ProgressBar";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
+        <DefaultProgressBar/>
         <Projects />
       </div>
     </main>
