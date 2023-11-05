3b83383f9a2a26a46e348861228999d13ebc0680
Lyla <146758880+NLyla@users.noreply.github.com>
2023-11-05 12:48:11 +0700

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -13,7 +13,7 @@ const SkillText = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Think better with Next js 13
+            Think it's better to use react
           </h1>
         </motion.div>
         <motion.div
@@ -24,7 +24,7 @@ const SkillText = () => {
         </motion.div>
         <motion.div
         variants={slideInFromRight(0.5)}
-        className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
+        className='text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
         >
             Never miss a task, deadline or idea
         </motion.div>
